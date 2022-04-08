677bb178cce2f1a3e7acfb8e8387511e7126c5f3
Junhan0037 <wnsgks0037@naver.com>
2022-04-08 15:10:18 +0900

    fix: clean up

--- src/main/java/com/springbatch/repository/StoreHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springbatch.repository;
-
-import com.springbatch.model.StoreHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StoreHistoryRepository extends JpaRepository<StoreHistory, Long> {
-}
